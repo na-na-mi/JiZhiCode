f09ab1276daf7ef70a8d0437de77efc86a0d1e03
帅气的机智君 <ljz400216@gmail.com>
2022-09-04 17:07:23 +0800

    make them no warnings

--- 网速测试.py
+++ 网速测试.py
@@ -1,7 +1,14 @@
 import speedtest
-s = speedtest.Speedtest()
-s.get_best_server()
-dl = s.download()
-ul = s.upload()
-print(f"下载速度：{dl/1024/1024:.2f}Mb")
-print(f"上传速度：{ul/1024/1024:.2f}Mb")
+
+
+def main():
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    dl = s.download()
+    ul = s.upload()
+    print(f"下载速度：{dl / 1024 / 1024:.2f}Mb")
+    print(f"上传速度：{ul / 1024 / 1024:.2f}Mb")
+
+
+if __name__ == '__main__':
+    main()
