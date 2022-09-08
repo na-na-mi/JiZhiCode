dcac72838eed61211fc9515508ac25bb114fed91
帅气的机智君 <ljz400216@gmail.com>
2022-09-08 10:08:49 +0800

    增强程序的鲁棒性

--- 依照关键字爬取百度百科.py
+++ 依照关键字爬取百度百科.py
@@ -1,19 +1,24 @@
 import re
 import requests as rq
 
+
 def get_baidubaike():
+    try:
+        keyword = input('请输入你要查找的关键字:')
+        url = 'https://baike.baidu.com/item/{}'.format(keyword)
+        Header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/96.0.4664.110 Safari/537.36 "
+        }
+        html = rq.get(url, headers=Header).content.decode('utf-8')
 
-    keyword = input('请输入你要查找的关键字:')
-    url = 'http://baike.baidu.com/item/{}'.format(keyword)
-    Header={
-     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
-     }  
-    html = rq.get(url , headers = Header).content.decode('utf-8')
+        regex = re.compile('content="(.*?)">')
+        text = re.findall(regex, html)[0]
+        return text
+    except:
+        return "产生异常"
 
-    regex = re.compile('content="(.*?)">')
-    words = re.findall(regex, html)[0]
-    return words
-  
+
 if __name__ == '__main__':
     words = get_baidubaike()
     print(words)
