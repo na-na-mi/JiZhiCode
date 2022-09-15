477ceab19610f892aa7034f7c463b4d0751f5099
帅气的机智君 <ljz400216@gmail.com>
2022-09-15 14:23:13 +0800

    regex test

---
+++ work.py
@@ -0,0 +1,22 @@
+import re
+
+str = '''<span class="td td-2nd"><a href="/air/nanchang.html" target="_blank">南昌市</a></span><span class="td 
+td-4rd">17</span><span class="td td-4rd"><em class="f1" style="color:#79b800">优</em></span> '''
+print(re.findall(r"\d\d", str))
+print(re.findall(r"[6789]\d", str))
+print(re.findall(r"td.", str))
+s = 'aaa,bbb ccc high'
+print(re.findall(r'[a-zA-Z]+', s))
+print(re.findall(r"\d{2,}", str))
+print(re.findall(r"\d{1,}b\d{1,}", str))
+str2 = "fdfd<a>aaaaa</a>dfefe<b>bbbb</b>"
+regex = '<.*?>.*?<\/.*?>'
+print(re.findall(regex, str2))
+
+str3 = "::ljz@yznu.edu.cn"
+regex = '::([a-z]{2,13})@([a-z]{2,13})\.(com|edu|net|org|gov|yznu|)'
+print(re.findall(regex, str3))
+
+print(re.findall(r"td", str))
+print(re.findall(r"^td", str))
+
